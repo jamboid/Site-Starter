adf2ad1373520f807ae53b5c64489c6413758bda
Jamie Boyd <jamie@wearegood.com>
2013-11-14 14:34:03 +0000

    More progress in configuring Grunt-based pipeline

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,38 @@ module.exports = function(grunt) {
   // Load packages
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('node-bourbon');
-
-  // Default task(s).
-  grunt.registerTask('default', ['watch']);
+  grunt.loadNpmTasks('grunt-imageoptim');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Project configuration.
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
+
+    // Default task - Build order
+    grunt.registerTask('default',
+    [
+      'jshint',
+
+      // start new build
+      //'htmlizr:prod',
+
+      'watch',
+
+
+      // minimize JavaScript to ./build/assets/js/
+      'uglify:prod',
+
+      // optimise SVG in ./build/assets/img/
+      //'svgmin:prod',
+
+      // rasterize SVG in ./build/assets/img/
+      //'svg2png:prod',
+
+      // optimise images in ./build/assets/img/
+      'imageoptim:prod'
+    ]);
 
     // Metadata.
     meta: {
@@ -20,6 +43,7 @@ module.exports = function(grunt) {
       srcPath: 'assets/scss/',
       deployPath: 'assets/css/'
     },
+
 
     // Task configuration.
     sass: {
@@ -30,6 +54,36 @@ module.exports = function(grunt) {
         },
         files: {
           '<%= meta.deployPath %>screen.css': '<%= meta.srcPath %>screen.scss'
+        }
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'tasks/**/*.js']
+    },
+
+    uglify: {
+      options: {
+        preserveComments: 'none'
+      },
+      prod: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['assets/js/**/*.js', "!assets/js/**/*.min.js"],
+            dest: 'build/assets/',
+            ext: '.min.js'
+          }
+        ]
+      }
+    },
+
+    imageoptim: {
+      prod: {
+        src: ['assets/img/, assets/css/img'],
+        options: {
+          quitAfter: true
         }
       }
     },
