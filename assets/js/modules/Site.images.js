f95898cd22b58ce7e15d8107799828bf49f59a25
Jamie Boyd <jamie@wearegood.com>
2014-01-03 15:06:59 +0000

    Converted Site.images module to object-based

--- assets/js/modules/Site.images.js
+++ assets/js/modules/Site.images.js
@@ -10,95 +10,97 @@ Site.images = (function ($) {
   var defaults = { },
       $lazyLoadImages = $('.lazyLoader'),
 
-      // Display a pre-loaded lazy image, adding atrributes set on
-      // the sprite container
-      displaySpriteImageInContainer = function (container, image) {
-        var $thisContainer = $(container),
-            $thisImage = $(image),
-            imageAlt = $thisContainer.data('alt') || 'image',
-            imageWidth = $thisContainer.data('width') || '';
+      // LazyImage object
+      LazyImage = function (elem) {
+        var $thisSprite = $(elem),
 
-        $thisImage.attr('width', imageWidth).attr('alt', imageAlt);
-        $thisContainer.prepend($thisImage).addClass('imageLoaded');
-        // Need to allow browser a moment to process the addition of the image before diplaying it
-        window.setTimeout(function () {$thisContainer.addClass('imageDisplayed');}, 100);
-        Site.utils.cl("Image loaded and displayed");
+            // Display a pre-loaded lazy image, adding atrributes set on
+            // the sprite container
+            displaySpriteImageInContainer = function (image) {
+              var $thisContainer = $thisSprite,
+                  $thisImage = $(image),
+                  imageAlt = $thisContainer.data('alt') || 'image',
+                  imageWidth = $thisContainer.data('width') || '';
+
+              $thisImage.attr('width', imageWidth).attr('alt', imageAlt);
+              $thisContainer.prepend($thisImage).addClass('imageLoaded');
+              // Need to allow browser a moment to process the addition of the image before diplaying it
+              window.setTimeout(function () {$thisContainer.addClass('imageDisplayed');}, 100);
+            },
+
+            // Create and preload a new image based on a sprite src
+            // then call a function once the image is loaded into memory
+            getSpriteImageFile = function () {
+              var thisImageUrl = $thisSprite.data('src'),
+                  imageToAdd = new Image();
+
+                  imageToAdd.src = thisImageUrl;
+                  $(imageToAdd).imagesLoaded(displaySpriteImageInContainer(imageToAdd));
+            },
+
+            loadSpriteImageIfInView = function () {
+              if(Site.utils.isElementInView($thisSprite)){
+                getSpriteImageFile($thisSprite);
+              }
+            },
+
+            // lazyImage class
+            buildLazyImage = function () {
+              var loadingMethod = $thisSprite.data('loading');
+
+              if(loadingMethod === 'click') {
+                // Do nothing
+              }
+              // If image is set to display when container is in view
+              else if (loadingMethod === 'view') {
+                // Load image if it is in view
+                loadSpriteImageIfInView($thisSprite);
+              }
+              // Otherwise load the image on page load
+              else {
+                getSpriteImageFile($thisSprite);
+              }
+
+              bindCustomMessageEvents();
+              subscribeToEvents();
+            },
+
+            // Add event handler for main navigation toggle
+            bindCustomMessageEvents = function () {
+              $thisSprite.on('loadLazyImage', function (e) {
+                e.preventDefault();
+                loadSpriteImageIfInView();
+              });
+            },
+
+            subscribeToEvents = function () {
+
+            };
+
+        this.loadLazyImage = function () {
+          loadSpriteImageIfInView();
+        };
+
+        this.init = function () {
+          buildLazyImage();
+        };
       },
 
-      // Create and preload a new image based on a sprite src
-      // then call a function once the image is loaded into memory
-      getSpriteImageFile = function (sprite) {
-        var $thisSprite = sprite,
-            thisImageUrl = $thisSprite.data('src'),
-            imageToAdd = new Image();
-
-            imageToAdd.src = thisImageUrl;
-            $(imageToAdd).imagesLoaded(displaySpriteImageInContainer($thisSprite, imageToAdd));
-      },
-
-      loadSpriteImageIfInView = function (sprite) {
-        var $thisSprite = sprite;
-        if(Site.utils.isElementInView($thisSprite)){
-          getSpriteImageFile($thisSprite);
-        }
-      },
-
-      // lazyImage class
-      buildLazyImage = function (sprite) {
-        var $thisSprite = $(sprite),
-            loadingMethod = $thisSprite.data('loading');
-
-        if(loadingMethod === 'click') {
-          // Do nothing
-        }
-        // If image is set to display when container is in view
-        else if (loadingMethod === 'view') {
-          // Load image if it is in view
-          loadSpriteImageIfInView($thisSprite);
-        }
-        // Otherwise load the image on page load
-        else {
-          getSpriteImageFile($thisSprite);
-        }
-      },
-
-      // Set up all lazy images on the page
-      buildLazyImages = function () {
+      setLazyLoadImages = function () {
         $lazyLoadImages.each(function () {
-          buildLazyImage(this);
+          var thisLazyImage = new LazyImage(this);
+          thisLazyImage.init();
         });
-      },
-
-      // Exposed function to update images when the page layout changes
-      // - checks if image is loaded already
-      updateLazyImages = function () {
-        $lazyLoadImages.each(function () {
-          var $thisSprite = $(this);
-          if(!$thisSprite.hasClass('imageLoaded') && $thisSprite.data('loading') === 'view' ) {
-            loadSpriteImageIfInView($thisSprite);
-          }
-        });
-      },
-
-      // Exposed function to load/display image when it is clicked on
-      //
-      loadClickedImage = function (sprite) {
-        var $thisSprite = $(sprite);
-        if(!$thisSprite.hasClass('imageLoaded') && $thisSprite.data('loading') === 'click' ) {
-          getSpriteImageFile($thisSprite);
-        }
       },
 
       init = function () {
         Site.utils.cl("Site.images initialised");
-        buildLazyImages();
+        setLazyLoadImages();
       };
 
   // Return Public API
   return {
-    init: init,
-    updateLazyImages: updateLazyImages,
-    loadClickedImage: loadClickedImage
+    init: init
   };
 
 }(jQuery));
