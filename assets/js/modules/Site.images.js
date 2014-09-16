8cdc876534fa67013ba9f99365a2135bec516471
Jamie Boyd <jamie@wearegood.com>
2014-09-16 10:03:02 +0100

    Updates to Carousel, Image and Layout JS Modules

--- assets/js/modules/Site.images.js
+++ assets/js/modules/Site.images.js
@@ -12,22 +12,24 @@ Site.images = (function ($) {
   ///////////////
 
   var defaults = { },
-      lazyImageSel = $('[data-image-load]'),
+      smartImageSel = '[data-image-load]',
 
   //////////////////
   // Constructors //
   //////////////////
 
       /**
-       * Creates a LazyImage object to manage a lazy-loaded image component
+       * Creates a SmartImage object to manage a lazy-loaded image component
        * @constructor
        */
-      LazyImage = function (elem) {
+      SmartImage = function (elem) {
         var $thisSprite = $(elem),
-            $placeholderImage = $thisSprite.find('img.placeholder').eq(0),
+            $placeholderImage = $thisSprite.find('> img').eq(0),
             loadingMethod = $thisSprite.data('image-load'),
             imageType = $thisSprite.data('image-type') || 'inline',
+            imageTargetSel = $thisSprite.data('image-target') || null,
             imageLoaded = false,
+            imageReloader = false,
             imageToAdd = new Image(),
 
             // Display a pre-loaded lazy image, adding atrributes set on
@@ -41,7 +43,14 @@ Site.images = (function ($) {
               if($placeholderImage.length > 0) {
                 $placeholderImage.attr('src', $thisImage.attr('src')).removeClass('placeholder').removeAttr('width').removeAttr('height');
               } else {
-                $thisSprite.prepend($thisImage);
+
+                if(imageTargetSel !== null){
+                  $thisSprite.parent().find(imageTargetSel).eq(0).append($thisImage);
+                } else {
+                  $thisSprite.prepend($thisImage);
+                }
+
+                $placeholderImage = $thisImage;
               }
 
               $thisSprite.addClass('imageLoaded');
@@ -61,7 +70,6 @@ Site.images = (function ($) {
                   spriteBackgroundColor = $thisSprite.data('background-color');
 
               $thisSprite.addClass('imageLoaded').css('background-image', spriteImage).addClass('flexImage').addClass(spriteBackgroundPos).css('background-color', spriteBackgroundColor) ;
-              Site.utils.cl('image loaded in background');
               imageLoaded = true;
             },
 
@@ -77,9 +85,9 @@ Site.images = (function ($) {
                   imageToAdd.src = thisImageUrl;
 
                 if(imageType === 'inline') {
-                  $(imageToAdd).imagesLoaded(displaySpriteImageInContainer(imageToAdd));
+                  $(imageToAdd).imagesLoaded(displaySpriteImageInContainer(imageToAdd)).done(function() { $.publish('layout/change'); });
                 } else if (imageType === 'background') {
-                  $(imageToAdd).imagesLoaded(displaySpriteImageAsBackground(thisImageUrl));
+                  $(imageToAdd).imagesLoaded(displaySpriteImageAsBackground(thisImageUrl)).done(function() { $.publish('layout/change'); });
                 }
             },
 
@@ -89,11 +97,11 @@ Site.images = (function ($) {
              */
             loadSpriteImageIfInView = function () {
               if(imageType === 'inline') {
-                if(Site.utils.isElementInView($thisSprite) && imageLoaded === false){
+                if(Site.utils.isElementInView($thisSprite) && (imageLoaded === false || imageReloader === true)){
                   getSpriteImageFile($thisSprite);
                 }
               } else if (imageType === 'background') {
-                if(Site.utils.isElementInView($thisSprite.parent()) && imageLoaded === false){
+                if(Site.utils.isElementInView($thisSprite.parent()) && (imageLoaded === false || imageReloader === true)){
                   getSpriteImageFile($thisSprite);
                 }
               }
@@ -111,6 +119,13 @@ Site.images = (function ($) {
                   loadSpriteImageIfInView($thisSprite);
                 }
               });
+
+              $thisSprite.on('reloadImage', function (e) {
+                e.preventDefault();
+                if(imageLoaded === true && imageReloader === true){
+                  getSpriteImageFile($thisSprite);
+                }
+              });
             },
 
             /**
@@ -118,15 +133,14 @@ Site.images = (function ($) {
              * @function
              */
             subscribeToEvents = function () {
-
-              Site.utils.cl(loadingMethod);
-
               if(loadingMethod !== 'click' && loadingMethod !== 'pageload') {
                 $.subscribe('page/scroll', function () {$(this).trigger('loadLazyImage');},$thisSprite);
                 $.subscribe('page/resize', function () {$(this).trigger('loadLazyImage');},$thisSprite);
                 $.subscribe('layout/change', function () {$(this).trigger('loadLazyImage');},$thisSprite);
                 $.subscribe('page/load', function () {$(this).trigger('loadLazyImage');},$thisSprite);
               }
+
+              $.subscribe('breakpoint/change', function () {$(this).trigger('reloadImage');},$thisSprite);
             };
 
         /**
@@ -134,6 +148,10 @@ Site.images = (function ($) {
          * @function
          */
         this.init = function () {
+
+          if($thisSprite.hasClass('reLoader')) {
+            imageReloader = true;
+          }
 
           if(loadingMethod === 'click') {
             // Do nothing
@@ -161,6 +179,45 @@ Site.images = (function ($) {
         };
       },
 
+      /**
+       * Creates an SmartImageManager object to manage dynamic creation of SmartImage objects
+       * @constructor
+       */
+      SmartImageManager = function () {
+
+        var
+
+        /**
+         * Initalise new SmartImage objects to manage newly created smart image components
+         * @function
+         */
+        createNewSmartImageObjects = function (elems) {
+          var newSmartImages = $(elems).find(smartImageSel);
+          $(newSmartImages).each(function () {
+            var thisSmartImage = new SmartImage(this);
+            thisSmartImage.init();
+          });
+        },
+
+        /**
+         * Subscribe object to Global Messages
+         * @function
+         */
+        subscribeToEvents = function () {
+          $.subscribe('content/change', function (topic,data) { createNewSmartImageObjects(data); });
+        };
+
+        /**
+         * Initialise this object
+         * @function
+         */
+        this.init = function () {
+          subscribeToEvents();
+        };
+      },
+
+
+
   ///////////////
   // Functions //
   ///////////////
@@ -180,10 +237,14 @@ Site.images = (function ($) {
       init = function () {
         Site.utils.cl("Site.images initialised");
 
-        $(lazyImageSel).each(function () {
-          var thisLazyImage = new LazyImage(this);
-          thisLazyImage.init();
+        $(smartImageSel).each(function () {
+          var thisSmartImage = new SmartImage(this);
+          thisSmartImage.init();
         });
+
+        // Create a new SmartImageManager object
+        var thisSmartImageManager = new SmartImageManager();
+        thisSmartImageManager.init();
 
         delegateEvents();
       };
--- assets/js/modules/Site.layout.js
+++ assets/js/modules/Site.layout.js
@@ -12,6 +12,7 @@ Site.layout = (function ($) {
   ///////////////
 
   var responsiveSize = 'small',
+      newSize,
 
 
   //////////////////
@@ -24,6 +25,20 @@ Site.layout = (function ($) {
        */
       ResponsiveLayoutManager = function () {
         var
+
+        /**
+         * Publish a message when the breakpoint changes
+         * @function
+         */
+        checkBreakpointChange = function (newSize) {
+          if (responsiveSize !== newSize) {
+
+            responsiveSize = newSize;
+            $.publish('breakpoint/change');
+          } else {
+            responsiveSize = newSize;
+          }
+        },
 
         /**
          * Set and update the responsiveSize variable based on current screen width
@@ -46,19 +61,24 @@ Site.layout = (function ($) {
               responsiveSize = 'static';
               break;
             case(screenWidth <= screenSizeIs.small):
-              responsiveSize = 'small';
+              newSize = 'small';
+              checkBreakpointChange(newSize);
               break;
             case(screenWidth <= screenSizeIs.medium):
-              responsiveSize = 'medium';
+              newSize = 'medium';
+              checkBreakpointChange(newSize);
               break;
             case(screenWidth <= screenSizeIs.large):
-              responsiveSize = 'large';
+              newSize = 'large';
+              checkBreakpointChange(newSize);
               break;
             case(screenWidth <= screenSizeIs.xlarge):
-              responsiveSize = 'xlarge';
+              newSize = 'xlarge';
+              checkBreakpointChange(newSize);
               break;
             case(screenWidth > screenSizeIs.xlarge):
-              responsiveSize = 'xxlarge';
+              newSize = 'xxlarge';
+              checkBreakpointChange(newSize);
               break;
           }
         },
@@ -68,7 +88,7 @@ Site.layout = (function ($) {
          * @function
          */
         subscribeToEvents = function () {
-          $.subscribe('debouncedresize', function () { updateResponsiveSize(); });
+          $.subscribe('page/resize', function () { updateResponsiveSize(); });
         };
 
         /**
